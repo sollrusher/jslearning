765964dd36fdffce99af7e78d3de43754ed314d5
Danila <sollrusher@gmail.com>
2020-07-18 20:34:32 +0300

    Внедрение функций

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 "use strict";
 
 let numberOfFilms;
-numberOfFilms=+prompt("Сколько фильмов вы посмотрели? ","");
+
+
+function start(){
+    numberOfFilms=+prompt("Сколько фильмов вы посмотрели? ","");
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+    
+        numberOfFilms=+prompt("Сколько фильмов вы посмотрели? ","");
+        
+    }
+
+}
+start();
+
 
 let personalMovieDB= {
 
@@ -11,6 +24,8 @@ let personalMovieDB= {
     genres:[],
     privat: false
 };
+
+function level (){
 
 if(personalMovieDB.count <10)
 {
@@ -25,23 +40,42 @@ else if(personalMovieDB.count>30)
     alert("Вы киноман");
 }
 else{alert("Произошла ошибка");}
-   
+}
 
+level();
+
+
+function qA(){
 for (let i=0; i<2; i++)
 {
 
     let fw1=prompt(`Фильм?`,``),
         oc1=prompt(`Оценка`,``);
 
-        if(fw1!=null && oc1!=null && fw1.length!="" && oc1.length!="" && fw1.length<50 && oc1.length <50){
+        if(fw1!=null && oc1!=null && fw1!="" && oc1!="" && fw1.length<50 && oc1.length <50){
         
         personalMovieDB.movies[fw1]=oc1;
         }
 else{i--;}
+}
+}
+ qA();
 
+function showMyDB (hidden)
+{
+    if (!hidden)
+    {
+        console.log(personalMovieDB); 
+    }
 }
 
-console.log(personalMovieDB);  
 
+function writeYouGenres (){
 
+    for(let i=1;i<=3;i++){
+personalMovieDB.genres[i-1]= prompt(`Ваш любимый жанр под номером- ${i}`, "");
 
+    }
+}
+writeYouGenres();
+showMyDB(personalMovieDB.privat);
